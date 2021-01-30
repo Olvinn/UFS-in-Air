7b701505132f8f3779b1c6169eb63f25fbffa59d
Sergey <Sergeybas97@gmail.com>
2021-01-30 22:21:07 +0500

    StartMenu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [System.Obsolete]
+    public void JoinGame()
+    {
+        Application.LoadLevel(3);
+    }
+
+    [System.Obsolete]
+    public void VolumeSettings()
+    {
+        Application.LoadLevel(1);
+    }
+
+    [System.Obsolete]
+    public void Exit()
+    {
+        Application.LoadLevel(2);
+    }
+}
---
+++ Assets/Scripts/Menu/SoundsMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsMenu : MonoBehaviour
+{
+    public static void OnMusicValueChanged()
+    {
+        
+    }
+    public static void OnAmbienceValueChanged()
+    {
+        
+    }
+    public static void OnStepsValueChanged()
+    {
+        
+    }
+    public static void OnScreamsValueChanged()
+    {
+
+    }
+
+    [System.Obsolete]
+    public void ButtonBack()
+    {
+        Application.LoadLevel(0);
+    }
+}
